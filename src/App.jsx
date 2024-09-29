422d18c3eff58902b4bba9f7ec8874189959cb26
masivDieter <dieter@mercedesandsingh.com>
2024-09-29 16:16:05 -0600

    code to hide addressbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useAuth } from "./context/UserContext";
 // import MessagesList from "./components/Chat/MessagesList/MessagesList";
@@ -7,7 +7,23 @@ import Chat from "./Chat/Chat";
 import Authentication from "./Chat/Signin/Authentication";
 function App() {
   const { googleSignIn, logout, currentUser, dbUser } = useAuth();
+  const goFullScreen = () => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    }
+  };
+  useEffect(() => {
+    const hideAddressBar = () => {
+      window.scrollTo(0, 1);
+    };
 
+    // Hide the address bar after the component is mounted
+    window.addEventListener("load", hideAddressBar);
+
+    return () => {
+      window.removeEventListener("load", hideAddressBar);
+    };
+  }, []);
   return (
     <>
       {/* <div className="text-black">hello</div> */}
@@ -18,6 +34,7 @@ function App() {
         Logout
       </div> */}
       {/* <MessagesList /> */}
+      {/* <button onClick={goFullScreen}>Go Fullscreen</button> */}
       <Routes>
         <Route path="/" element={currentUser ? <Chat /> : <Authentication />} />
       </Routes>
